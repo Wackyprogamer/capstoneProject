3fd4c9da93bf46abdc4ef0bd5a6ef7bb7ebe47c2
Sergio Castillo <sergio.castillo5392@stu.mtec.edu>
2024-02-23 08:15:22 -0700

    added css file and beginning structure for courses page

--- src/components/ReactRoutes.jsx
+++ src/components/ReactRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
+import Courses from "./courses";//why no work? Works now when changed to lowercase
 
 export const ReactRoutes = () => {
   return (
@@ -8,6 +9,7 @@ export const ReactRoutes = () => {
       <Route path="/" exact Component={Login}></Route>
       <Route path="/LogIn" exact Component={Login}></Route>
       <Route path="/SignUp" exact Component={Signup}></Route>
+      <Route path="/courses" exact Component={Courses}></Route>
     </Routes>
   );
 };
---
+++ src/components/courses.jsx
@@ -0,0 +1,25 @@
+
+import Logo from "../images/Registration_App_Logo.png"
+import Button from "@mui/material/Button"
+import '../css/courses.css'; 
+//import { ReactRoutes } from "./components/ReactRoutes";
+//This is for user course page
+
+function Courses() {
+  return (
+    <div className="Courses">
+        <h1 id="userWelcome">Welcome User!</h1>
+   
+
+    <footer id="typicalFooter" className="w-full h-[25px] flex justify-center items-center absolute bottom-0">
+        <p>Help</p>
+        <p>@2023 - MtecPro</p>
+        <p>About</p>
+    </footer>
+    
+    </div>
+
+  );
+}
+
+export default Courses;
